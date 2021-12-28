83a92c3aa9ea5d48198a16e3d062e335432f815c
Juliet <agutu@gmail.com>
2021-12-28 18:59:38 +0300

    add li to js

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,14 @@ $(document).ready(function() {
         var inputtedFirstName = $("input#new-first-name").val();
         var inputtedLastName = $("input#new-last-name").val();
 
-        var newContact = new Contact( inputtedFirstName, inputtedLastName);
+        var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-        $("ul#contacts").append("<li><span class='contact'>")+newContact.firstName "</span></li>");
+        $("ul#contacts").append("<li><span class='contact'>"+ newContact.firstName + "</span></li>");
 
-        $("input#new-first-name").val("");
-        $("input#new-last-name").val("");
+        $("input#new-first-name").val(" ");
+        $("input#new-last-name").val(" ");
       });
+
+
+});
     
