c9b2dc8a5f3aaf5040e49f55970ccdab73a0b222
Juliet <agutu@gmail.com>
2021-12-28 17:57:17 +0300

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,9 @@ $(document).ready(function() {
 
         var newContact = new Contact( inputtedFirstName, inputtedLastName);
 
-        $(ul#contacts)
-    }
-}
+        $("ul#contacts").append("<li><span class='contact'>")+newContact.firstName "</span></li>");
+
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
+      });
+    
